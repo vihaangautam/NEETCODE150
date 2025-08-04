4e8fd594b3902f4fd68c0d678c6e0ba740e4bedf
Vihaan Gautam <vihaangautam04@gmail.com>
2025-08-04 19:57:20 +0530

    initial commit

---
+++ ContainsDuplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasDuplicate(int[] nums) {
+        Set<Integer> entry= new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(entry.contains(nums[i])){
+                return true;
+            }
+            entry.add(nums[i]);
+        }
+        return false;
+
+        
+    }
+}
